a094a5cbeb43ea399d371c00b3b3cfcbe7fffaa2
FunkyMonkOnKo <115803617+FunkyMonkOnKo@users.noreply.github.com>
2023-02-09 13:27:52 +0100

    unlocking characters

--- Assets/Scripts/Player/PlayerModelPickerButton.cs
+++ Assets/Scripts/Player/PlayerModelPickerButton.cs
@@ -1,8 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerModelPickerButton : MonoBehaviour
 {
   [SerializeField] private string modelName;
+  public int modelPrice;
+  public bool isUnlocked = false;
+
+  [SerializeField] private Button unlockButton;
+
+  private void Start()
+  {
+    if (PlayerPrefs.HasKey(name) || modelPrice <= 0) {
+      isUnlocked = true;     
+    }
+
+    UpdateUnlocks();
+  }
+
+  public void UnlockCharacter() {
+    int actualCoins = PlayerPrefs.GetInt(ScoreController.coinsCountHash);
+
+    PlayerPrefs.SetInt(ScoreController.coinsCountHash, actualCoins - modelPrice);
+    PlayerPrefs.SetInt(name, 1);
+
+    isUnlocked = true;
+
+    UpdateUnlocks();
+  }
+
+  private void UpdateUnlocks() {
+
+    if (!isUnlocked)
+    {
+      GetComponent<Button>().interactable = false;
+
+      unlockButton.interactable = true;
+
+      if (PlayerPrefs.GetInt(ScoreController.coinsCountHash) < modelPrice)
+      {
+        unlockButton.interactable = false;
+      }
+
+    }
+    else
+    {
+      GetComponent<Button>().interactable = true;
+      unlockButton.gameObject.SetActive(false);
+    }
+  }
 }
