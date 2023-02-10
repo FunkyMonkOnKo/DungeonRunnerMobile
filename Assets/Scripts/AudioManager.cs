54f65100927f49d3a9e62b9de3cd9334f47c861a
FunkyMonkOnKo <115803617+FunkyMonkOnKo@users.noreply.github.com>
2023-02-10 13:45:07 +0100

    Audio preparation

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+  public static AudioManager instance;
+  private void Awake()
+  {
+    if (instance == null)
+    {
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  public AudioSource mainMenuMusic, levelMusic;
+
+  public AudioSource[] sfx;
+
+  public void PlayMainMenuMusic()
+  {
+    levelMusic.Stop();
+
+    mainMenuMusic.Play();
+  }
+
+  public void PlayLevelMusic()
+  {
+    if (!levelMusic.isPlaying)
+    {
+      mainMenuMusic.Stop();
+
+      levelMusic.Play();
+    }
+  }
+
+  public void PlaySFX(int sfxToPlay)
+  {
+    sfx[sfxToPlay].Stop();
+    sfx[sfxToPlay].Play();
+  }
+
+  public void PlaySFXAdjusted(int sfxToAdjust)
+  {
+    sfx[sfxToAdjust].pitch = Random.Range(.8f, 1.2f);
+    PlaySFX(sfxToAdjust);
+
+  }
+}
