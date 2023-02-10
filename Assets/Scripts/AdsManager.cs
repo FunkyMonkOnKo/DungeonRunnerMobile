6d1675c5cbfb07700e4274a254ebd71484c93a93
FunkyMonkOnKo <115803617+FunkyMonkOnKo@users.noreply.github.com>
2023-02-10 13:56:03 +0100

    cleanup adsmanager

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -56,41 +56,27 @@ public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnity
   // Load content to the Ad Unit:
   public void LoadAd()
   {
-    // IMPORTANT! Only load content AFTER initialization 
     Debug.Log("Loading Ad: " + _adUnitId);
     Advertisement.Load(_adUnitId, this);
   }
 
-  // Show the loaded content in the Ad Unit:
   public void ShowAd()
   {
-    // Note that if the ad content wasn't previously loaded, this method will fail
     Debug.Log("Showing Ad: " + _adUnitId);
     Advertisement.Show(_adUnitId, this);
   }
 
   // Implement Load Listener and Show Listener interface methods: 
-  public void OnUnityAdsAdLoaded(string adUnitId)
-  {
-    // Optionally execute code if the Ad Unit successfully loads content.
-  }
+  public void OnUnityAdsAdLoaded(string adUnitId) {  }
 
   public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
   {
     Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
-    // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
-
-    GameController.instance.RestartGame();
-    SceneManager.LoadScene(1);
   }
 
   public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
   {
     Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
-    // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
-
-    GameController.instance.RestartGame();
-    SceneManager.LoadScene(1);
   }
 
   public void OnUnityAdsShowStart(string adUnitId) { }
