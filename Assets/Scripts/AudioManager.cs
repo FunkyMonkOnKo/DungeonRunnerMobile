f2c8ee2b5c0c9009e86ea88ac50656e2fe649a8f
FunkyMonkOnKo <115803617+FunkyMonkOnKo@users.noreply.github.com>
2023-02-13 15:13:15 +0100

    Mute button add

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
   public static AudioManager instance;
+
   private void Awake()
   {
     if (instance == null)
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
   [SerializeField] private TMP_Text highScoreTextUI;
   [SerializeField] private TMP_Text coinsCountTextUI;
+
+  [SerializeField] private Button MuteButton;
+  [SerializeField] private Sprite MuteOnImage;
+  [SerializeField] private Sprite MuteOffImage;
+  bool isMute;
 
   private void Start()
   {
@@ -36,6 +42,14 @@ public class MainMenu : MonoBehaviour
   {
     AudioManager.instance.PlaySFX(4);
     SceneManager.LoadScene(3);
+  }
+
+  public void Mute()
+  {
+    isMute = !isMute;
+    AudioListener.volume = isMute ? 0 : 1;
+
+    MuteButton.GetComponent<Image>().sprite = isMute ? MuteOnImage : MuteOffImage;
   }
 
   public void DeletePlayerPrefs()
